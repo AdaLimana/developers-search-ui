1981a7b0a99db3451b541a94e2736002d8e2507c
Adair J. Limana <ada_limana@yahoo.com.br>
2021-03-22 03:25:40 -0300

    feat: adicionado o AuthGuard nas rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppLoginComponent } from './app-login/app-login.component';
 import { AppMainComponent } from './app-main/app-main.component';
 import { CandidatoCreateComponent } from './candidato/candidato-create/candidato-create.component';
 import { CandidatoListComponent } from './candidato/candidato-list/candidato-list.component';
 import { CandidatoUpdateComponent } from './candidato/candidato-update/candidato-update.component';
+import { RecrutadorUpdateComponent } from './recrutador/recrutador-update/recrutador-update.component';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: AppMainComponent,
+    canActivate: [AuthGuard],
     children: [
       {path: '', component: CandidatoListComponent},
       {path: 'candidatos', component: CandidatoListComponent},
       {path: 'candidatos/create', component: CandidatoCreateComponent},
       {path: 'candidatos/update/:id', component: CandidatoUpdateComponent},
+      {path: 'recrutadores/update/:id', component: RecrutadorUpdateComponent},
     ]
-  }
+  },
+  { path: 'authentication', component: AppLoginComponent},
 ];
 
 @NgModule({
